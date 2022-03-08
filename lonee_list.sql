325b586eeddefc7cfc8439d2f951de63d24addd4
Tashi Tsering KC <43880377+PosthumasFrankestein@users.noreply.github.com>
2022-03-08 19:32:11 +0545

    Update lonee_list.sql

--- lonee_list.sql
+++ lonee_list.sql
@@ -60,19 +60,6 @@ CREATE TABLE `lonee` (
 -- Dumping data for table `lonee`
 --
 
-INSERT INTO `lonee` (`l_no`, `name`, `mobile`, `email`, `amount`, `date`) VALUES
-(1, 'akhilesh tamang', '982222222222', 'tamangrocks@gmail.com', 50000, '2019-02-04'),
-(2, 'Bikas Tamang', '794724414', 'bikastok4@gmail.com', 50000, '2020-10-14'),
-(3, 'Tashi Tsering K.C', '87821764', 'chheringtashi@gmail.com', 10000, '2020-12-08'),
-(4, 'Stalker Bikas', '777777777', 'icanseeyou@gmail.com', 20000, '2020-11-10'),
-(5, 'Bikas Stalker', '412921421', 'iamoverpole@gmail.com', 300000, '2020-12-01'),
-(6, 'Samir Moktan', '2141243', 'moktansamir@yahoo.com', 30000, '2019-10-16'),
-(8, 'noimad', '98126141', 'adminnoimad@gmail.com', 10000, '2021-02-06'),
-(24, 'noimantt', '983333333', 'noimantt@gmail.com', 1200, '2021-02-02'),
-(25, 'noiceboi123', '78589', 'jj@gmail.com', 20000, '2021-02-05'),
-(26, 'Som pd shrestha', '98723131', 'sunway.som@gmail.com', 10000, '2021-02-11'),
-(27, 'Tenzin', '12414', 'tenzin@ymail.com', 11000, '2021-03-29');
-
 --
 -- Indexes for dumped tables
 --
